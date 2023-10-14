8f962fba0ec0d5b63488c24eaf1207a4812c7a62
Anik Podder <110771733+anikpodder@users.noreply.github.com>
2023-10-14 15:52:48 +0600

    Create 1. Even Odd 1.cpp

---
+++ 1. Even Odd 1.cpp
@@ -0,0 +1,33 @@
+// Author : Anik Podder
+// CF Handle : anikpodder
+#include<bits/stdc++.h>
+using namespace std;
+
+#define Fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define endl "\n"
+
+void solve()
+{
+    int n;
+    
+    cin >> n;
+    
+    if(n & 1) 
+        cout << "odd" << endl;
+    else
+        cout << "even" << endl;
+}
+
+int main()
+{
+    Fast
+
+    int t;
+    
+    cin >> t;
+    
+    while(t--){
+
+         solve();
+    }
+}
